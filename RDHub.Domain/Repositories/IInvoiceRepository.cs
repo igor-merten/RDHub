9226610d76c5a8c87b483b70ff0196b0f5e6c9f9
igormerten <igormeerten@gmail.com>
2026-04-27 15:21:33 -0300

    Adicionado repositorios no domain

---
+++ RDHub.Domain/Repositories/IInvoiceRepository.cs
@@ -0,0 +1,12 @@
+﻿
+using RDHub.Domain.Aggregates;
+using System.Text;
+
+namespace RDHub.Domain.Repositories;
+
+public interface IInvoiceRepository
+{
+    Task<Invoice?> GetByIdAsync(Guid id, CancellationToken ct = default);
+    Task AddAsync(Invoice invoice, CancellationToken ct = default);
+    Task UpdateAsync(Invoice invoice, CancellationToken ct = default);
+}
---
+++ RDHub.Domain/Repositories/IPixChargeRepository.cs
@@ -0,0 +1,11 @@
+﻿using RDHub.Domain.Aggregates;
+using RDHub.Domain.ValueObjects;
+
+namespace RDHub.Domain.Repositories;
+
+public interface IPixChargeRepository
+{
+    Task<PixCharge?> GetByIdAsync(TxId txId, CancellationToken ct = default);
+    Task AddAsync(PixCharge pixCharge, CancellationToken ct = default);
+    Task UpdateAsync(PixCharge pixCharge, CancellationToken ct = default);
+}
