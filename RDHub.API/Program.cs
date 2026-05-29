e9ed6deff4736ba2a4a88d78d93569ea2bdb820a
igormerten <igormeerten@gmail.com>
2026-05-29 09:27:55 -0300

    Injeção de dependencia em arquivo separado para alinhar com documentação

--- RDHub.API/Program.cs
+++ RDHub.API/Program.cs
@@ -1,16 +1,8 @@
 using MediatR;
-using Microsoft.EntityFrameworkCore;
-using RDHub.Application.Interfaces;
+using Microsoft.AspNetCore.Builder;
 using RDHub.API.Middleware;
-using RDHub.Domain.Repositories;
-using RDHub.Infrastructure.BankAdapters;
-using RDHub.Infrastructure.BankAdapters.Abstractions;
-using RDHub.Infrastructure.BackgroundServices;
-using RDHub.Infrastructure.Messaging;
-using RDHub.Infrastructure.Persistence;
-using RDHub.Infrastructure.Persistence.Repositories;
 using RDHub.Application.Commands.CreateCob;
-using RDHub.Infrastructure.BankAdapters.MockBank;
+using RDHub.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -19,67 +11,17 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddProblemDetails();
 builder.Services.AddSwaggerGen();
-builder.Services.AddHttpClient("RecebDigital", client =>
-    client.BaseAddress = new Uri(builder.Configuration["RecebaDigital:BaseUrl"]!));
 
 // ====== MEDIATR ======
 builder.Services.AddMediatR(cfg =>
     cfg.RegisterServicesFromAssembly(typeof(CreateCobCommand).Assembly));
 
 // ====== DATABASE ======
-builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddMediatR(cfg =>
+    cfg.RegisterServicesFromAssembly(typeof(CreateCobCommand).Assembly));
 
-// ====== REPOSITORIES ======
-builder.Services.AddScoped<IAccountRepository, AccountRepository>();
-builder.Services.AddScoped<ICredentialRepository, CredentialRepository>();
-builder.Services.AddScoped<IPixKeyRepository, PixKeyRepository>();
-builder.Services.AddScoped<IAuditRepository, AuditRepository>();
-builder.Services.AddScoped<IMessageRepository, MessageRepository>();
-builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
-
-// ====== BANK ADAPTERS ======
-//adapter1
-builder.Services.AddSingleton<IMockBankTokenProvider, MockTokenProvider>();
-
-builder.Services.AddHttpClient(nameof(MockBankAdapter), client =>
-{
-    client.BaseAddress = new Uri(builder.Configuration["BanksBaseUrl:MockServer"]!);
-});
-
-builder.Services.AddSingleton<IBankPixAdapter>(sp =>
-{
-    var httpClientFactory = sp.GetRequiredService<IHttpClientFactory>();
-    var http = httpClientFactory.CreateClient(nameof(MockBankAdapter));
-    var tokenProvider = sp.GetRequiredService<IMockBankTokenProvider>();
-    var bankId = builder.Configuration["BankAdapters:MockBankId"]!;
-
-    return new MockBankAdapter(http, tokenProvider, bankId);
-});
-
-
-//adapter2
-//builder.Services.AddHttpClient<MockBank2Adapter>(client =>
-//    client.BaseAddress = new Uri(builder.Configuration["BanksBaseUrl:MockServer"]!));
-//builder.Services.AddSingleton<IBankPixAdapter>(sp =>
-//{
-//    var http = sp.GetRequiredService<IHttpClientFactory>().CreateClient(nameof(MockBank2Adapter));
-//    var bankId = builder.Configuration["BankAdapters:MockBank2Id"]!;
-//    return new MockBank2Adapter(http, bankId);
-//});
-
-builder.Services.AddSingleton<IBankAdapterFactory, BankAdapterFactory>();
-
-// ====== MESSAGING ======
-builder.Services.AddSingleton<IMessageQueue>(sp =>
-    new RabbitMqPublisher(
-        builder.Configuration["RabbitMQ:HostName"]!,
-        builder.Configuration["RabbitMQ:UserName"]!,
-        builder.Configuration["RabbitMQ:Password"]!));
-
-// ====== BACKGROUND SERVICES ======
-builder.Services.AddHostedService<PaymentConsumerService>();
-builder.Services.AddHostedService<PaymentSchedulerService>();
+// ====== INFRASTRUCTURE ======
+builder.Services.AddInfrastructure(builder.Configuration);
 
 var app = builder.Build();
 
@@ -95,5 +37,4 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 app.UseAuthorization();
 app.MapControllers();
-
 app.Run();
---
+++ RDHub.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using RDHub.Application.Interfaces;
+using RDHub.Domain.Repositories;
+using RDHub.Infrastructure.BackgroundServices;
+using RDHub.Infrastructure.BankAdapters;
+using RDHub.Infrastructure.BankAdapters.Abstractions;
+using RDHub.Infrastructure.BankAdapters.MockBank;
+using RDHub.Infrastructure.Messaging;
+using RDHub.Infrastructure.Persistence;
+using RDHub.Infrastructure.Persistence.Repositories;
+
+namespace RDHub.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(
+        this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        // ====== DATABASE ======
+        services.AddDbContext<AppDbContext>(options =>
+            options.UseNpgsql(configuration.GetConnectionString("DefaultConnection")));
+
+        // ====== REPOSITORIES ======
+        services.AddScoped<IAccountRepository, AccountRepository>();
+        services.AddScoped<ICredentialRepository, CredentialRepository>();
+        services.AddScoped<IPixKeyRepository, PixKeyRepository>();
+        services.AddScoped<IAuditRepository, AuditRepository>();
+        services.AddScoped<IMessageRepository, MessageRepository>();
+        services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+        // ====== BANK ADAPTERS ======
+        services.AddSingleton<IMockBankTokenProvider, MockTokenProvider>();
+        services.AddHttpClient(nameof(MockBankAdapter), client =>
+        {
+            client.BaseAddress = new Uri(configuration["BanksBaseUrl:MockServer"]!);
+        });
+        services.AddSingleton<IBankPixAdapter>(sp =>
+        {
+            var factory = sp.GetRequiredService<IHttpClientFactory>();
+            var http = factory.CreateClient(nameof(MockBankAdapter));
+            var tokenProvider = sp.GetRequiredService<IMockBankTokenProvider>();
+            var bankId = configuration["BankAdapters:MockBankId"]!;
+            return new MockBankAdapter(http, tokenProvider, bankId);
+        });
+        services.AddSingleton<IBankAdapterFactory, BankAdapterFactory>();
+
+        // ====== MESSAGING ======
+        services.AddSingleton<IMessageQueue>(sp =>
+            new RabbitMqPublisher(
+                configuration["RabbitMQ:HostName"]!,
+                configuration["RabbitMQ:UserName"]!,
+                configuration["RabbitMQ:Password"]!));
+
+        // ====== BACKGROUND SERVICES ======
+        services.AddHostedService<PaymentConsumerService>();
+        services.AddHostedService<PaymentSchedulerService>();
+
+        return services;
+    }
+}
